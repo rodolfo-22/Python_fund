49f0cc6ac936bcbb26dfb8ed21dfbe0c9826c723
rodolfo <00082421@uca.edu.sv>
2024-10-16 18:15:54 -0600

    :zap::construction_worker: inti the firt excersice :sparkles:

---
+++ python_03/tarea/asignacion.py
@@ -0,0 +1,15 @@
+#Tareas: Completa los siguientes pasos usando pandas. Asegúrate de documentar tu código para que sea claro y comprensible.
+
+#Crea una Serie llamada precios con los precios de cierre de las acciones de las 
+# siguientes empresas: "AAPL", "GOOGL", "MSFT", "AMZN", "TSLA". Usa estos precios: 
+# 175.50, 2850.30, 310.20, 3555.72, 799.90.
+
+#Define los índices de la Serie con los nombres de las acciones de las empresas
+precios = pd.Series([175.50, 2850.30, 310.20, 3555.72, 799.90], index=["AAPL", "GOOGL", "MSFT", "AMZN", "TSLA"])
+
+
+
+
+
+
+
