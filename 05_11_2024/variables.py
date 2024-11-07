e799af7b42513e75446abde14b94dce9f77b3c55
rodolfo <00082421@uca.edu.sv>
2024-11-07 01:19:00 -0600

    :boom: another clas of python

---
+++ 05_11_2024/variables.py
@@ -0,0 +1,45 @@
+#entero
+num = 10
+#flotante
+num2 = 10.5
+#cadena
+cadena = "Hola Mundo"
+#booleano
+booleano = True
+#lista
+lista = [1,2,3,4,5]
+#tupla
+tupla = (1,2,3,4,5)
+#diccionario
+diccionario = {"nombre":"Juan","edad":25}
+#nulo
+nulo = None
+
+#---------------------------------------------------------
+#operadores aritmeticos
+#suma
+suma = 5 + 5
+#resta
+resta = 5 - 5
+#multiplicacion
+multiplicacion = 5 * 5
+#division
+division = 5 / 5
+#modulo, nos da el residuo de la division
+modulo = 5 % 5
+#potencia
+potencia = 5 ** 5
+#division entera
+division_entera = 5 // 5
+
+#---------------------------------------------------------
+#concatenacion
+cadena1 = "Hola"
+cadena2 = "Mundo"
+concatenacion = cadena1 + " " + cadena2
+print(concatenacion)
+print(cadena1 + " 500")
+
+#---------------------------------------------------------
+
+
---
+++ python_04/26-10-2024.py
@@ -0,0 +1,10 @@
+#analissi exploratorio de datos
+
+#las variables cualitativas se ecpresan mejor en graficos de barra, de sectores (muestra la proporcion de cada
+#categoria respecto al total)  de mosaico (muestra la proporcion de cada categoria respecto al total y respecto a otra variable)
+
+#las cuantitativas  se representan mejor en histogramas, diagramas de caja, grafico de dispersion
+# y grafico de lineas
+
+#analissi exploratorio de datos, es el proceso de analizar conjuntos de datps para resumir sus principales
+#caracteristicas.
